993bb45319c0342f5fcfecc4ff2cb20c6db9929d
Your Namede <edoardo.monti.bo@gmail.com>
2023-04-16 18:01:46 +0200

    inizio snack 3

--- snack3/assets/main.js
+++ snack3/assets/main.js
@@ -1,9 +1,19 @@
 /*
 
 Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b).
-l'array egnerato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
+l'array generato da parte vostra dovrà essere di ad esempio nomi o cose: ['alberto', 'michele','angelo','simone']
 se come numeri ad esemio ho 2 e 4 devo inserire in un array nuovo l'elemento in posizione 3
 La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri.
 Usiamo i nuovi metodi degli array foreach o filter.
 
 */
+
+const array = ["luca", "marco", "livo", "artu", "spina"]
+
+function myFunctio(array,numberUno, numberdue){
+    array.forEach((element, index) => {
+        console.log(element)
+    });
+}
+
+console.log(myFunctio())
